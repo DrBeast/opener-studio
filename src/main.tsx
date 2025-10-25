a484a171b3ac322bdd40887083cf1cdf96f50988
Aleksandr Rakitin <aleksandr.rakitin@gmail.com>
2025-10-25 21:54:17 +0000

    Installed PostHog with basic setup. Use .env.local to disable tracking while testing locally. Use Vercel env vars to track deployed environments.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,28 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { PostHogProvider } from "posthog-js/react";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = createRoot(document.getElementById("root")!);
+
+const shouldTrack = import.meta.env.VITE_POSTHOG_ENABLED === "true";
+
+root.render(
+  <React.StrictMode>
+    {shouldTrack ? (
+      <PostHogProvider
+        apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+        options={{
+          api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+          capture_exceptions: true,
+          debug: import.meta.env.MODE === "development",
+        }}
+      >
+        <App />
+      </PostHogProvider>
+    ) : (
+      <App />
+    )}
+  </React.StrictMode>
+);
