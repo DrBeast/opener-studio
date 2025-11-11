8d7d3aa0dba578705fd461fe2c9d6fd489ad44b1
Aleksandr Rakitin <aleksandr.rakitin@gmail.com>
2025-11-11 21:23:19 +0000

    Removed footer in regged exp - more space!

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,16 +1,19 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { useAuth } from "@/hooks/useAuth";
 
 interface MainLayoutProps {
   children: React.ReactNode;
 }
 
 const MainLayout = ({ children }: MainLayoutProps) => {
+  const { user } = useAuth();
+
   return (
     <div className="h-screen flex flex-col overflow-hidden">
       <Header />
       <main className="flex-1 min-h-0 overflow-y-auto">{children}</main>
-      <Footer />
+      {!user && <Footer />}
     </div>
   );
 };
